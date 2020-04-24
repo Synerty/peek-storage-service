efcb3541672a56f8495e96b8a9b6b179d6113d64
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-04-24 18:56:28 +1000

    Initial commit
    PEEK-179

---
+++ publish.settings.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_storage"
+PYPI_PUBLISH="1"
+
+GITHUB_PUSH="${GITHUB_PUSH:-1}"
+
+VER_FILES_TO_COMMIT=""
+
+VER_FILES=""
+
---
+++ rename_plugin.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+
+# remove the git index
+[ -f .git/index ] && rm .git/index || true
+
+rm -rf dist *egg-info || true
+
+find ./ -name '__pycache__' -exec rm -rf {} \; || true
+
+
+set nounset
+set errexit
+
+function replace {
+    from=$1
+    to=$2
+
+    for d in `find ./ -depth -type d`
+    do
+        new=`echo $d | sed "s/$from/$to/g"`
+        [ "$d" != "$new" ] && mv $d $new
+    done
+
+    for f in `find ./ -type f`
+    do
+        new=`echo $f | sed "s/$from/$to/g"`
+        [ "$f" != "$new" ] && mv $f $new
+    done
+
+    for f in `find ./ -type f -not -name '.git' -not -name 'rename_plugin.sh'`
+    do
+        sed -i "s/$from/$to/g" $f
+    done
+
+}
+
+# RENAME THE PLUGIN
+replace "PeekServer"  "PeekStorage"
+replace "peekServer" "peekStorage"
+
+# Remove compile generated javascript
+find ./ -type f -not -name '.git' -name "*.js" -exec rm {} \; || true
+find ./ -type f -not -name '.git' -name "*.js.map" -exec rm {} \; || true
